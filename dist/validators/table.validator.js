580689e44d4f50318835c1f4aa36f716a774a00e
sonpasavee975 <sunpasavee@gmail.com>
2026-06-04 11:52:45 +0700

    Remove dist from version control

--- dist/validators/table.validator.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createTableSchema = void 0;
-const zod_1 = require("zod");
-exports.createTableSchema = zod_1.z.object({
-    number: zod_1.z.number().int().positive(),
-});
